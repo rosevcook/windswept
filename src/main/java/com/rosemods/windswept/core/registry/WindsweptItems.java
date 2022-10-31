7ae890c2c86101d4aa3a7241d2c732142da4f6f6
Rose Cook <96022374+rosevcook@users.noreply.github.com>
2022-10-31 17:19:57 +0000

    Project files
    -updated build gradle
    -removed forge credits and changlog files
    -fixed broken logo.png file
    -added github licence and issue tracker to mods.toml
    -updated pack.mcmeta

--- src/main/java/com/rosemods/windswept/core/registry/WindsweptItems.java
+++ src/main/java/com/rosemods/windswept/core/registry/WindsweptItems.java
@@ -67,8 +67,8 @@ public class WindsweptItems {
 	// Music Discs //
 
 	//change the length in seconds
-	public static final RegistryObject<Item> MUSIC_DISC_RAIN = HELPER.createItem("music_disc_rain", () -> new BlueprintRecordItem(2, WindsweptSounds.MUSIC_DISC_RAIN::get, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 1));
-	public static final RegistryObject<Item> MUSIC_DISC_SNOW = HELPER.createItem("music_disc_snow", () -> new BlueprintRecordItem(2, WindsweptSounds.MUSIC_DISC_SNOW::get, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 1));
+	public static final RegistryObject<Item> MUSIC_DISC_RAIN = HELPER.createItem("music_disc_rain", () -> new BlueprintRecordItem(2, WindsweptSounds.MUSIC_DISC_RAIN::get, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 125));
+	public static final RegistryObject<Item> MUSIC_DISC_SNOW = HELPER.createItem("music_disc_snow", () -> new BlueprintRecordItem(2, WindsweptSounds.MUSIC_DISC_SNOW::get, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 125));
 
 	// Spawn Eggs //
 	
