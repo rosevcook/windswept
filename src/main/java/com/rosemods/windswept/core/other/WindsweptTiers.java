a9543d1d6da51a1037566520e0c6ee38cab58a64
rosevcook <rose.cook969@gmail.com>
2023-03-03 08:16:07 +0000

    snow boots now only have 1 armour point

--- src/main/java/com/rosemods/windswept/core/other/WindsweptTiers.java
+++ src/main/java/com/rosemods/windswept/core/other/WindsweptTiers.java
@@ -9,5 +9,5 @@ import net.minecraft.world.item.crafting.Ingredient;
 
 public final class WindsweptTiers {
 	public static final BlueprintArmorMaterial SNOW_BOOTS = new BlueprintArmorMaterial(Windswept.REGISTRY_HELPER.prefix("snow_boots"), 
-			12, new int[]{2, 3, 2, 3}, 17, () -> SoundEvents.ARMOR_EQUIP_GOLD, 0f, 0f, () -> Ingredient.of(Items.LEATHER));
+			12, new int[]{1, 0, 0, 0}, 17, () -> SoundEvents.ARMOR_EQUIP_GOLD, 0f, 0f, () -> Ingredient.of(Items.LEATHER));
 }
