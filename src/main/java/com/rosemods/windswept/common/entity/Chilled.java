de61848a84984fb05119f2d571a8ab2074f08a3d
rosevcook <rose.cook969@gmail.com>
2023-10-24 22:00:05 +0100

    chilled spawn with silver equipment (cnc)

--- src/main/java/com/rosemods/windswept/common/entity/Chilled.java
+++ src/main/java/com/rosemods/windswept/common/entity/Chilled.java
@@ -1,5 +1,6 @@
 package com.rosemods.windswept.common.entity;
 
+import com.rosemods.windswept.core.other.WindsweptConstants;
 import com.rosemods.windswept.core.registry.WindsweptEnchantments;
 import com.rosemods.windswept.core.registry.WindsweptEntities;
 import com.rosemods.windswept.core.registry.WindsweptItems;
@@ -13,9 +14,11 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.fml.ModList;
 
 public class Chilled extends Zombie {
 
@@ -56,8 +59,32 @@ public class Chilled extends Zombie {
     protected void populateDefaultEquipmentSlots(RandomSource rand, DifficultyInstance difficulty) {
         super.populateDefaultEquipmentSlots(rand, difficulty);
 
-        if (this.random.nextFloat() < (this.level.getDifficulty() == Difficulty.HARD ? .5f : .33f))
-            this.setItemSlot(EquipmentSlot.FEET, this.random.nextBoolean() ? Items.LEATHER_BOOTS.getDefaultInstance() : WindsweptItems.SNOW_BOOTS.get().getDefaultInstance());
+        if (ModList.get().isLoaded(WindsweptConstants.CAVERNS_AND_CHASMS))
+            cncCompat(rand);
+
+        if (rand.nextFloat() < (this.level.getDifficulty() == Difficulty.HARD ? .5f : .33f))
+            this.setItemSlot(EquipmentSlot.FEET, rand.nextBoolean() ? Items.LEATHER_BOOTS.getDefaultInstance() : WindsweptItems.SNOW_BOOTS.get().getDefaultInstance());
+    }
+
+    private void cncCompat(RandomSource random) {
+        if (random.nextFloat() < .075f)
+            this.setItemSlot(EquipmentSlot.MAINHAND, randomDurability(random, random.nextBoolean() ? WindsweptConstants.SILVER_SWORD.get() : WindsweptConstants.SILVER_SHOVEL.get()));
+        else if (random.nextFloat() < .05f) {
+            this.setItemSlot(EquipmentSlot.MAINHAND, randomDurability(random, WindsweptConstants.SILVER_AXE.get()));
+            this.setItemSlot(EquipmentSlot.OFFHAND, randomDurability(random, WindsweptConstants.SILVER_AXE.get()));
+        }
+
+        if (random.nextFloat() < .05f) this.setItemSlot(EquipmentSlot.HEAD, randomDurability(random, WindsweptConstants.SILVER_HELMET.get()));
+        if (random.nextFloat() < .05f) this.setItemSlot(EquipmentSlot.CHEST, randomDurability(random, WindsweptConstants.SILVER_CHESTPLATE.get()));
+        if (random.nextFloat() < .05f) this.setItemSlot(EquipmentSlot.LEGS, randomDurability(random, WindsweptConstants.SILVER_LEGGINGS.get()));
+        if (random.nextFloat() < .05f) this.setItemSlot(EquipmentSlot.FEET, randomDurability(random, WindsweptConstants.SILVER_BOOTS.get()));
+    }
+
+    private static ItemStack randomDurability(RandomSource random, Item item) {
+        ItemStack stack = item.getDefaultInstance();
+        stack.setDamageValue(random.nextInt(1, stack.getMaxDamage()));
+
+        return stack;
     }
 
     @Override
--- src/main/java/com/rosemods/windswept/core/other/WindsweptConstants.java
+++ src/main/java/com/rosemods/windswept/core/other/WindsweptConstants.java
@@ -14,6 +14,14 @@ public final class WindsweptConstants {
     public static final String NEAPOLITAN = "neapolitan";
     public static final String FARMERSDELIGHT = "farmersdelight";
     public static final String AUTUMNITY = "autumnity";
+    public static final String CAVERNS_AND_CHASMS = "caverns_and_chasms";
 
     public static final Supplier<Item> FOUL_BERRIES = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("autumnity", "foul_berries"));
+    public static final Supplier<Item> SILVER_SWORD = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_sword"));
+    public static final Supplier<Item> SILVER_SHOVEL = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_shovel"));
+    public static final Supplier<Item> SILVER_AXE = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_axe"));
+    public static final Supplier<Item> SILVER_HELMET = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_helmet"));
+    public static final Supplier<Item> SILVER_CHESTPLATE = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_chestplate"));
+    public static final Supplier<Item> SILVER_LEGGINGS = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_leggings"));
+    public static final Supplier<Item> SILVER_BOOTS = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_boots"));
 }
