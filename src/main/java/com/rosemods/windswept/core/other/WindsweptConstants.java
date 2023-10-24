2ea2aa1479d11f19357821bbb79c90ad4493fbbe
rosevcook <rose.cook969@gmail.com>
2023-10-24 19:07:23 +0100

    autumnity compat fix thingy

--- src/main/java/com/rosemods/windswept/core/other/WindsweptConstants.java
+++ src/main/java/com/rosemods/windswept/core/other/WindsweptConstants.java
@@ -7,12 +7,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.function.Supplier;
 
 public final class WindsweptConstants {
-	public static final String QUARK = "quark";
-	public static final String BERRY_GOOD = "berry_good";
-	public static final String WOODWORKS = "woodworks";
-	public static final String BOATLOAD = "boatload";
-	public static final String NEAPOLITAN = "neapolitan";
-	public static final String FARMERSDELIGHT = "farmersdelight";
+    public static final String QUARK = "quark";
+    public static final String BERRY_GOOD = "berry_good";
+    public static final String WOODWORKS = "woodworks";
+    public static final String BOATLOAD = "boatload";
+    public static final String NEAPOLITAN = "neapolitan";
+    public static final String FARMERSDELIGHT = "farmersdelight";
+    public static final String AUTUMNITY = "autumnity";
 
-	public static final Supplier<Item> FOUL_BERRIES = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("autumnity", "foul_berries"));
+    public static final Supplier<Item> FOUL_BERRIES = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("autumnity", "foul_berries"));
 }
