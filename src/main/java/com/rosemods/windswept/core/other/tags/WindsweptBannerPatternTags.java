f7166c37262203eadcf52b0debd997c943dd420c
rosevcook <rose.cook969@gmail.com>
2023-11-24 04:10:24 +0000

    do you guys even look at these?

--- src/main/java/com/rosemods/windswept/core/other/tags/WindsweptBannerPatternTags.java
+++ src/main/java/com/rosemods/windswept/core/other/tags/WindsweptBannerPatternTags.java
@@ -6,12 +6,12 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.entity.BannerPattern;
 
 public final class WindsweptBannerPatternTags {
-    public static final TagKey<BannerPattern> SNOW_CHARGE = createTag("pattern_item/snow_charge");
-    public static final TagKey<BannerPattern> SNOW_GOLEM = createTag("pattern_item/snow_golem");
-    public static final TagKey<BannerPattern> ROSE_FLOWER = createTag("pattern_item/rose_flower");
+    public static final TagKey<BannerPattern> SNOW_CHARGE = createTag("snow_charge");
+    public static final TagKey<BannerPattern> SNOW_GOLEM = createTag("snow_golem");
+    public static final TagKey<BannerPattern> ROSE_FLOWER = createTag("rose_flower");
 
     private static TagKey<BannerPattern> createTag(String name) {
-        return TagKey.create(Registry.BANNER_PATTERN_REGISTRY, Windswept.REGISTRY_HELPER.prefix(name));
+        return TagKey.create(Registry.BANNER_PATTERN_REGISTRY, Windswept.REGISTRY_HELPER.prefix("pattern_item/" + name));
     }
 
 }
