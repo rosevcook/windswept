4203601a4731f6bc59a27088b75f9be2582c11f6
rosevcook <rose.cook969@gmail.com>
2023-11-27 23:23:24 +0000

    more more holly textures

--- src/main/java/com/rosemods/windswept/core/other/WindsweptConstants.java
+++ src/main/java/com/rosemods/windswept/core/other/WindsweptConstants.java
@@ -7,14 +7,14 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.function.Supplier;
 
 public final class WindsweptConstants {
-    public static final String QUARK = "minecraft";
-    public static final String BERRY_GOOD = "minecraft";
-    public static final String WOODWORKS = "minecraft";
-    public static final String BOATLOAD = "minecraft";
-    public static final String NEAPOLITAN = "minecraft";
-    public static final String FARMERSDELIGHT = "minecraft";
-    public static final String AUTUMNITY = "minecraft";
-    public static final String CAVERNS_AND_CHASMS = "minecraft";
+    public static final String QUARK = "quark";
+    public static final String BERRY_GOOD = "berry_good";
+    public static final String WOODWORKS = "woodworks";
+    public static final String BOATLOAD = "boatload";
+    public static final String NEAPOLITAN = "neapolitan";
+    public static final String FARMERSDELIGHT = "farmersdelight";
+    public static final String AUTUMNITY = "autumnity";
+    public static final String CAVERNS_AND_CHASMS = "caverns_and_chasms";
 
     public static final Supplier<Item> FOUL_BERRIES = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("autumnity", "foul_berries"));
     public static final Supplier<Item> SILVER_SWORD = () -> ForgeRegistries.ITEMS.getValue(new ResourceLocation("caverns_and_chasms", "silver_sword"));
