602409068d9366452f06c66c7f21c2e8938d8a50
rosevcook <rose.cook969@gmail.com>
2023-12-18 04:48:19 +0000

    Update GuiMixin.java

--- src/main/java/com/rosemods/windswept/core/mixin/client/GuiMixin.java
+++ src/main/java/com/rosemods/windswept/core/mixin/client/GuiMixin.java
@@ -1,14 +1,16 @@
 package com.rosemods.windswept.core.mixin.client;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.rosemods.windswept.core.Windswept;
+import com.rosemods.windswept.core.registry.WindsweptBlocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.Inventory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Gui.class)
 public class GuiMixin {
@@ -16,10 +18,12 @@ public class GuiMixin {
     @Shadow
     protected Minecraft minecraft;
 
-    @Inject(method = "render", at = @At("HEAD"))
-    private void render(PoseStack poseStack, float p_93032_, CallbackInfo info) {
-        //if (this.minecraft.player != null && this.minecraft.options.getCameraType().isFirstPerson() && !this.minecraft.player.isScoping()) //&& this.minecraft.player.getInventory().getArmor(3).is(WindsweptBlocks.CARVED_PINECONE_BLOCK.get().asItem()))
-        //((Gui) (Object) this).renderTextureOverlay(Windswept.location("textures/misc/pineconeblur.png"), 1f);
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getInventory()Lnet/minecraft/world/entity/player/Inventory;"))
+    private Inventory render(LocalPlayer instance) {
+        if (this.minecraft.player.getInventory().getArmor(3).is(WindsweptBlocks.CARVED_PINECONE_BLOCK.get().asItem()))
+            ((Gui) (Object) this).renderTextureOverlay(Windswept.location("textures/misc/pineconeblur.png"), 1f);
+
+        return this.minecraft.player.getInventory();
     }
 
 }
