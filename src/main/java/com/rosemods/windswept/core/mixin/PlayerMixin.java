97a2bde65ad81305525d3df3edb03b68d17d99ea
rosevcook <rose.cook969@gmail.com>
2023-12-27 21:43:28 +0000

    frozen waters

--- src/main/java/com/rosemods/windswept/core/mixin/PlayerMixin.java
+++ src/main/java/com/rosemods/windswept/core/mixin/PlayerMixin.java
@@ -1,13 +1,10 @@
 package com.rosemods.windswept.core.mixin;
 
 import com.rosemods.windswept.core.WindsweptConfig;
-import net.minecraft.core.Holder;
-import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.common.Tags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,12 +19,8 @@ public abstract class PlayerMixin extends LivingEntity {
 
     @Inject(method = "aiStep", at = @At("TAIL"))
     private void aiStep(CallbackInfo info) {
-        if (!this.level.isClientSide && !this.isInPowderSnow && !this.isDeadOrDying() && this.canFreeze() && this.isUnderWater() && WindsweptConfig.COMMON.freezingOceans.get()) {
-            Holder<Biome> biome = this.level.getBiome(this.blockPosition());
-
-            if (biome.is(Tags.Biomes.IS_SNOWY) && (biome.is(BiomeTags.IS_OCEAN) || biome.is(BiomeTags.IS_RIVER)))
-                this.setTicksFrozen(this.getTicksFrozen() + (this.random.nextBoolean() ? 2 : 3));
-        }
+        if (!this.level.isClientSide && !this.isInPowderSnow && !this.isDeadOrDying() && this.canFreeze() && this.isUnderWater() && WindsweptConfig.COMMON.freezingWater.get() && this.level.getBiome(this.blockPosition()).is(Tags.Biomes.IS_SNOWY))
+            this.setTicksFrozen(this.getTicksFrozen() + (this.random.nextBoolean() ? 2 : 3));
     }
 
 }
