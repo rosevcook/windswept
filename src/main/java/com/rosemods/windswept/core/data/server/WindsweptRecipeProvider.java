6c391843658d59de3bc155babd9e71bc4129b89c
rosevcook <rose.cook969@gmail.com>
2024-01-07 22:24:08 +0000

    fix recipe

--- src/main/java/com/rosemods/windswept/core/data/server/WindsweptRecipeProvider.java
+++ src/main/java/com/rosemods/windswept/core/data/server/WindsweptRecipeProvider.java
@@ -120,7 +120,7 @@ public class WindsweptRecipeProvider extends RecipeProvider {
         ShapelessRecipeBuilder.shapeless(GLAZED_GINGERBREAD_BRICKS.get()).requires(GINGERBREAD_BRICKS.get()).requires(Items.SUGAR).unlockedBy(getHasName(GINGERBREAD_BRICKS.get()), has(GINGERBREAD_BRICKS.get())).save(consumer, getSaveLocation("glazed_gingerbread_bricks_from_sugar"));
 
         ShapedRecipeBuilder.shaped(FEATHER_WING.get(), 2).define('#', PINECONE.get()).define('F', OWL_FEATHER.get()).pattern("#FF").unlockedBy(getHasName(OWL_FEATHER.get()), has(OWL_FEATHER.get())).save(consumer, getSaveLocation(FEATHER_WING.get()));
-        ShapedRecipeBuilder.shaped(FEATHER_ORNAMENT.get(), 2).define('#', PINECONE.get()).define('F', OWL_FEATHER.get()).pattern("##").pattern("FF").unlockedBy(getHasName(OWL_FEATHER.get()), has(OWL_FEATHER.get())).save(consumer, getSaveLocation(FEATHER_WING.get()));
+        ShapedRecipeBuilder.shaped(FEATHER_ORNAMENT.get(), 2).define('#', PINECONE.get()).define('F', OWL_FEATHER.get()).pattern("##").pattern("FF").unlockedBy(getHasName(OWL_FEATHER.get()), has(OWL_FEATHER.get())).save(consumer, getSaveLocation(FEATHER_ORNAMENT.get()));
 
         ShapelessRecipeBuilder.shapeless(POLISHED_DEEPSLATE_BUTTON.get()).requires(Items.POLISHED_DEEPSLATE).unlockedBy(getHasName(Items.POLISHED_DEEPSLATE), has(Items.POLISHED_DEEPSLATE)).save(consumer, getSaveLocation(POLISHED_DEEPSLATE_BUTTON.get()));
         ShapedRecipeBuilder.shaped(POLISHED_DEEPSLATE_PRESSURE_PLATE.get()).define('#', Items.POLISHED_DEEPSLATE).pattern("##").unlockedBy(getHasName(Items.POLISHED_DEEPSLATE), has(Items.POLISHED_DEEPSLATE)).save(consumer, getSaveLocation(POLISHED_DEEPSLATE_PRESSURE_PLATE.get()));
