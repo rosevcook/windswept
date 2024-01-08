1f6ca6c664d900091acba3edaea9ccdfaa91668b
rosevcook <rose.cook969@gmail.com>
2024-01-08 03:39:36 +0000

    Update DreamCatcherBlock.java

--- src/main/java/com/rosemods/windswept/common/block/DreamCatcherBlock.java
+++ src/main/java/com/rosemods/windswept/common/block/DreamCatcherBlock.java
@@ -4,17 +4,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -48,8 +49,24 @@ public class DreamCatcherBlock extends DoublePlantBlock {
     }
 
     @Override
-    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
-        return this.canSurvive(state, level, currentPos) ? state : Blocks.AIR.defaultBlockState();
+    public void playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
+        if (!level.isClientSide) {
+            if (player.isCreative()) {
+                if (state.getValue(HALF) == DoubleBlockHalf.LOWER) {
+                    BlockPos blockpos = pos.above();
+                    BlockState blockstate = level.getBlockState(blockpos);
+
+                    if (blockstate.is(state.getBlock()) && blockstate.getValue(HALF) == DoubleBlockHalf.UPPER) {
+                        level.setBlock(blockpos, Blocks.AIR.defaultBlockState(), 35);
+                        level.levelEvent(player, 2001, blockpos, Block.getId(blockstate));
+                    }
+                }
+            } else
+                dropResources(state, level, pos, null, player, player.getMainHandItem());
+        }
+
+        this.spawnDestroyParticles(level, player, pos, state);
+        level.gameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Context.of(player, state));
     }
 
 }
