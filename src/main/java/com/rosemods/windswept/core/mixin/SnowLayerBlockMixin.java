3e2cae2986e972c88befe7ab3e1bd30e1c8c787f
rosevcook <rose.cook969@gmail.com>
2024-02-17 13:21:38 +0000

    snow layer not melted by ice lantern

--- src/main/java/com/rosemods/windswept/core/mixin/SnowLayerBlockMixin.java
+++ src/main/java/com/rosemods/windswept/core/mixin/SnowLayerBlockMixin.java
@@ -1,11 +1,13 @@
 package com.rosemods.windswept.core.mixin;
 
 import com.rosemods.windswept.core.WindsweptConfig;
+import com.rosemods.windswept.core.registry.WindsweptBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SnowLayerBlock.class)
 public class SnowLayerBlockMixin {
-    @Inject(method = "randomTick", at = @At("TAIL"))
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
     private void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource rand, CallbackInfo info) {
         if (level.isRainingAt(pos) && WindsweptConfig.COMMON.rainWashSnow.get()) {
             int i = state.getValue(SnowLayerBlock.LAYERS);
@@ -22,8 +24,12 @@ public class SnowLayerBlockMixin {
                 level.removeBlock(pos, false);
             else
                 level.setBlock(pos, state.setValue(SnowLayerBlock.LAYERS, i - 1), 2);
-
         }
+
+        BlockPos.betweenClosedStream(new AABB(pos).inflate(2)).forEach(blockPos -> {
+            if (level.getBlockState(blockPos).is(WindsweptBlocks.ICE_LANTERN.get()))
+                info.cancel();
+        });
     }
 
 }
