b18e978c254e6b04f4ce1e5bd50f761e3b1ef7ba
rosevcook <rose.cook969@gmail.com>
2024-02-23 09:08:27 +0000

    ginger crop aabbs

--- src/main/java/com/rosemods/windswept/common/block/GingerCropBlock.java
+++ src/main/java/com/rosemods/windswept/common/block/GingerCropBlock.java
@@ -1,6 +1,8 @@
 package com.rosemods.windswept.common.block;
 
 import com.rosemods.windswept.core.registry.WindsweptItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
@@ -8,12 +10,26 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GingerCropBlock extends CropBlock {
     public static final IntegerProperty AGE = BlockStateProperties.AGE_4;
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+            Block.box(0f, 0f, 0f, 16f, 2f, 16f),
+            Block.box(0f, 0f, 0f, 16f, 4f, 16f),
+            Block.box(0f, 0f, 0f, 16f, 6f, 16f),
+            Block.box(0f, 0f, 0f, 16f, 12f, 16f),
+            Block.box(0f, 0f, 0f, 16f, 14f, 16f),
+    };
 
     public GingerCropBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_AGE[state.getValue(AGE)];
     }
 
     @Override
