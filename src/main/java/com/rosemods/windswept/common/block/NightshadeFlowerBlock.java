b595e7b2458a0cd9428f3ba2b576438d750b0118
rosevcook <rose.cook969@gmail.com>
2024-04-26 15:55:37 +0100

    Update NightshadeFlowerBlock.java

--- src/main/java/com/rosemods/windswept/common/block/NightshadeFlowerBlock.java
+++ src/main/java/com/rosemods/windswept/common/block/NightshadeFlowerBlock.java
@@ -1,9 +1,9 @@
 package com.rosemods.windswept.common.block;
 
-import com.rosemods.windswept.core.registry.WindsweptParticleTypes;
 import com.teamabnormals.blueprint.common.block.BlueprintFlowerBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.level.Level;
@@ -32,7 +32,7 @@ public class NightshadeFlowerBlock extends BlueprintFlowerBlock {
             double d2 = pos.getZ() + .55d - (rand.nextFloat() * .1f);
             double d3 = (.4f - (rand.nextFloat() + rand.nextFloat()) * .4f);
 
-            level.addParticle(WindsweptParticleTypes.WILL_O_THE_WISP.get(), d0 + Direction.UP.getStepX() * d3,
+            level.addParticle(ParticleTypes.END_ROD, d0 + Direction.UP.getStepX() * d3,
                     d1 + Direction.UP.getStepY() * d3, d2 + Direction.UP.getStepZ() * d3,
                     rand.nextGaussian() * 0.005D, rand.nextGaussian() * .005d, rand.nextGaussian() * .005d);
         }
