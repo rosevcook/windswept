a2f23e45b0e1ec68b0d517b5f4402ccb16de33fc
rosevcook <rose.cook969@gmail.com>
2024-05-15 15:55:34 +0100

    frostbiterwalker

--- src/main/java/com/rosemods/windswept/common/entity/animal/Frostbiter.java
+++ src/main/java/com/rosemods/windswept/common/entity/animal/Frostbiter.java
@@ -33,6 +33,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.enchantment.FrostWalkerEnchantment;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
@@ -199,6 +200,11 @@ public class Frostbiter extends TamableAnimal implements Endimatable, NeutralMob
         }
 
         return false;
+    }
+
+    @Override
+    protected void onChangedBlock(BlockPos pos) {
+        FrostWalkerEnchantment.onEntityMoved(this, this.level, pos, 2);
     }
 
     @Override
