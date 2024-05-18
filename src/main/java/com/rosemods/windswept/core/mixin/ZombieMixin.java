8046fb4df8901d225438a7032bcd44a82079904b
rosevcook <rose.cook969@gmail.com>
2024-05-18 13:30:07 +0100

    zombie lavender crown

--- src/main/java/com/rosemods/windswept/core/mixin/ZombieMixin.java
+++ src/main/java/com/rosemods/windswept/core/mixin/ZombieMixin.java
@@ -2,6 +2,8 @@ package com.rosemods.windswept.core.mixin;
 
 import com.rosemods.windswept.core.other.tags.WindsweptBiomeTags;
 import com.rosemods.windswept.core.registry.WindsweptBlocks;
+import com.rosemods.windswept.core.registry.WindsweptItems;
+import net.minecraft.core.Holder;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
@@ -9,6 +11,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,8 +25,14 @@ public class ZombieMixin extends Monster {
 
     @Inject(method = "populateDefaultEquipmentSlots", at = @At("TAIL"))
     public void populateDefaultEquipmentSlots(RandomSource rand, DifficultyInstance difficulty, CallbackInfo info) {
-        if (rand.nextFloat() < .1f && this.level.getBiome(this.blockPosition()).is(WindsweptBiomeTags.IS_PINE_BARRENS)) {
+        Holder<Biome> biome = this.level.getBiome(this.blockPosition());
+        boolean chance = rand.nextFloat() < .1f;
+        
+        if (chance && biome.is(WindsweptBiomeTags.IS_PINE_BARRENS)) {
             this.setItemSlot(EquipmentSlot.HEAD, WindsweptBlocks.CARVED_PINECONE_BLOCK.get().asItem().getDefaultInstance());
+            this.armorDropChances[EquipmentSlot.HEAD.getIndex()] = .5f;
+        } else if (chance && biome.is(WindsweptBiomeTags.IS_LAVENDER)) {
+            this.setItemSlot(EquipmentSlot.HEAD, WindsweptItems.LAVENDER_CROWN.get().getDefaultInstance());
             this.armorDropChances[EquipmentSlot.HEAD.getIndex()] = .5f;
         }
     }
