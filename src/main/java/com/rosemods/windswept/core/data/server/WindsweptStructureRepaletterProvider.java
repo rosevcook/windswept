342205fab4c4df90fb3e60e10b3cab30e201713e
rosevcook <rose.cook969@gmail.com>
2024-12-25 12:01:47 +0000

    FD crops in frozen village

--- src/main/java/com/rosemods/windswept/core/data/server/WindsweptStructureRepaletterProvider.java
+++ src/main/java/com/rosemods/windswept/core/data/server/WindsweptStructureRepaletterProvider.java
@@ -15,6 +15,7 @@ import net.minecraftforge.common.crafting.conditions.OrCondition;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.registries.ForgeRegistries;
+import vectorwing.farmersdelight.common.registry.ModBlocks;
 
 import static com.rosemods.windswept.core.registry.WindsweptBlocks.*;
 import static com.rosemods.windswept.core.registry.WindsweptStructures.*;
@@ -69,6 +70,7 @@ public class WindsweptStructureRepaletterProvider extends StructureRepaletterPro
 
         // Mod Compat //
         ICondition quarkOrWoodworks = new OrCondition(new ModLoadedCondition("quark"), new ModLoadedCondition("woodworks"));
+        ICondition farmersDelight = new ModLoadedCondition("farmersdelight");
 
         this.register(GROVE_WEATHERED_HOUSE, Blocks.BOOKSHELF, HOLLY_BOOKSHELF.get(), quarkOrWoodworks);
         this.register(GROVE_WEATHERED_HOUSE, Blocks.CHEST, HOLLY_CHEST.get(), quarkOrWoodworks);
@@ -77,6 +79,7 @@ public class WindsweptStructureRepaletterProvider extends StructureRepaletterPro
         this.register(VILLAGE_FROZEN, Blocks.BOOKSHELF, HOLLY_BOOKSHELF.get(), quarkOrWoodworks);
         this.register(VILLAGE_FROZEN, Blocks.CHEST, HOLLY_CHEST.get(), quarkOrWoodworks);
         this.register(VILLAGE_FROZEN, Blocks.LADDER, HOLLY_LADDER.get(), quarkOrWoodworks);
+        this.register(VILLAGE_FROZEN, Blocks.WHEAT, ModBlocks.CABBAGE_CROP.get(), farmersDelight);
         this.register(IGLOO, Blocks.CHEST, HOLLY_CHEST.get(), quarkOrWoodworks);
         this.register(IGLOO, Blocks.LADDER, HOLLY_LADDER.get(), quarkOrWoodworks);
         this.register(VILLAGE_SNOWY, Blocks.CHEST, CHESTNUT_CHEST.get(), quarkOrWoodworks);
