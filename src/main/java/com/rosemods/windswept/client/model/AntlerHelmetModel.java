60a0cc082d6b350ad3c5c0f51044a2df575059b0
rosevcook <rose.cook969@gmail.com>
2025-08-10 02:17:45 +0100

    fix antlers model

--- src/main/java/com/rosemods/windswept/client/model/AntlerHelmetModel.java
+++ src/main/java/com/rosemods/windswept/client/model/AntlerHelmetModel.java
@@ -23,7 +23,7 @@ public class AntlerHelmetModel extends HumanoidModel<LivingEntity> {
         PartDefinition root = meshdefinition.getRoot();
 
         PartDefinition helmet = root.addOrReplaceChild("helmet", CubeListBuilder.create().texOffs(0, 0).addBox(-4f, -8f, -4f, 8f, 8f, 8f, new CubeDeformation(.7f)), PartPose.offset(0f, 24f, 0f));
-        helmet.addOrReplaceChild("antlers", CubeListBuilder.create().texOffs(0, 15).addBox(-10f, -1.5f, -.25f, 30f, 14f, 0f, new CubeDeformation(0f)), PartPose.offsetAndRotation(-5f, -6f, 1f, .2618f, 0f, 0f));
+        helmet.addOrReplaceChild("antlers", CubeListBuilder.create().texOffs(0, 15).addBox(-10f, -10.5f, -.25f, 30f, 14f, 0f, new CubeDeformation(0f)), PartPose.offsetAndRotation(-5f, -6f, 1f, .2618f, 0f, 0f));
 
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
