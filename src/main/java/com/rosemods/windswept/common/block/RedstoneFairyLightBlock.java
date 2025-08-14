46d58cc01aea4f6e86396adbc912d6f1f6b7c9d9
rosevcook <rose.cook969@gmail.com>
2025-08-14 14:56:22 +0100

    redstone fairy light correctly powers blocks

--- src/main/java/com/rosemods/windswept/common/block/RedstoneFairyLightBlock.java
+++ src/main/java/com/rosemods/windswept/common/block/RedstoneFairyLightBlock.java
@@ -1,6 +1,7 @@
 package com.rosemods.windswept.common.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.DustParticleOptions;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -8,6 +9,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RedstoneTorchBlock;
@@ -15,6 +17,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class RedstoneFairyLightBlock extends PineconeBlock {
     public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
@@ -25,15 +29,10 @@ public class RedstoneFairyLightBlock extends PineconeBlock {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AMOUNT, LIT);
-    }
-
-    @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
         if (player.getItemInHand(hand).isEmpty()) {
             boolean lit = state.getValue(LIT);
-            level.setBlock(pos, state.setValue(LIT, !lit), 2);
+            level.setBlock(pos, state.setValue(LIT, !lit), 3);
             level.playSound(player, pos, lit ? SoundEvents.CHERRY_WOOD_PRESSURE_PLATE_CLICK_OFF : SoundEvents.CHERRY_WOOD_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS);
 
 
@@ -43,6 +42,22 @@ public class RedstoneFairyLightBlock extends PineconeBlock {
         return InteractionResult.PASS;
     }
 
+    @Override
+    public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return direction == Direction.DOWN && state.getValue(LIT) ? Math.min(state.getValue(AMOUNT) * 4, 15) : 0;
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState state) {
+        return state.getValue(LIT);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AMOUNT, LIT);
+    }
+
+    @OnlyIn(Dist.CLIENT)
     @Override
     public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
         if (state.getValue(LIT)) {
